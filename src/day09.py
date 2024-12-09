2fbbcf6b6acc4f0f36c702e2bed06de8cd526a2b
jschless <joe.c.schlessinger@gmail.com>
2024-12-09 09:51:54 -0500

    cleaned up

--- src/day09.py
+++ src/day09.py
@@ -1,60 +1,47 @@
 # fname = "./examples/day09.txt"
 fname = "./inputs/day09.txt"
 
-
 with open(fname) as f:
-    files, files_2, free, to_add = [], [], [], []
+    to_add = []
     flag = True
     f_id = 0
     for x in f.read().strip():
-        if flag:
-            to_add.append((f_id, int(x)))
-            files_2 += [len(files)] * int(x)
-            files.append(int(x))
-            f_id += 1
-        else:
-            to_add.append((-1, int(x)))
-            free.append(int(x))
+        to_add.append((f_id if flag else -1, int(x)))
+        f_id += 1 if flag else 0
         flag = not flag
-
-n_files = len(files_2)
-new_ans = []
-
-for file_id, size in enumerate(files):
-    new_ans += [file_id] * size
-    if file_id < len(free):
-        for _ in range(free[file_id]):
-            new_ans.append(files_2.pop())
 
 
 to_add_2 = [x for x in to_add]
-# ans = []
-# while len(to_add) > 0:
-#     f_id, size = to_add.pop(0)
-#     if f_id == -1:
-#         # fill gap with items from the end
-#         gap_size = size
-#         while gap_size > 0:
-#             f_id_end, size_end = to_add.pop()
-#             if f_id_end == -1:
-#                 # skip gaps
-#                 continue
-#             if size_end > size:
-#                 ans += [f_id_end] * gap_size
-#                 to_add.append((f_id_end, (size_end - gap_size)))
-#                 # add remainder back to end
-#                 gap_size = 0
-#             else:
-#                 ans += [f_id_end] * size_end
-#                 gap_size -= size_end
-#     else:
-#         ans += [f_id] * size
-
-
 p1, p2 = 0, 0
-# print(new_ans)
-for i, x in enumerate(new_ans[:n_files]):
-    p1 += i * x
+index = 0
+while len(to_add) > 0:
+    f_id, size = to_add.pop(0)
+    if f_id == -1:
+        # fill gap with items from the end
+        gap_size = size
+        while gap_size > 0:
+            f_id_end, size_end = to_add.pop()
+            if f_id_end == -1:
+                # skip gaps
+                continue
+            elif size_end > gap_size:
+                for i in range(gap_size):
+                    p1 += f_id_end * index
+                    index += 1
+                to_add.append((f_id_end, (size_end - gap_size)))
+                # add remainder back to end
+                gap_size = 0
+            else:
+                for i in range(size_end):
+                    p1 += f_id_end * index
+                    index += 1
+                gap_size -= size_end
+    else:
+        for i in range(size):
+            p1 += f_id * index
+            index += 1
+
+print("Part 1:", p1)
 
 
 def find_and_replace(files, space_avail):
@@ -72,7 +59,7 @@ def find_and_replace(files, space_avail):
     return to_insert
 
 
-ans_2 = []
+index = 0
 while len(to_add_2) > 0:
     f_id, size = to_add_2.pop(0)
     if f_id == -1:
@@ -82,19 +69,18 @@ while len(to_add_2) > 0:
             to_insert = find_and_replace(to_add_2, gap_size)
             if to_insert:
                 f_id_end, size_end = to_insert
-                ans_2 += [f_id_end] * size_end
+                for i in range(size_end):
+                    p2 += index * f_id_end
+                    index += 1
                 gap_size -= size_end
-            else:  # no candidates, fill remainder with -1
-                ans_2 += [-1] * gap_size
+            else:
+                index += gap_size  # don't need to add to total
                 gap_size = 0
 
     else:
-        ans_2 += [f_id] * size
-
-
-for i, x in enumerate(ans_2):
-    if x != -1:
-        p2 += i * x
+        for i in range(size):
+            p2 += index * f_id
+            index += 1
 
 print("Part 1:", p1)
 
