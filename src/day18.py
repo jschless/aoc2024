720faaeefe9c7da3d60d14b2c4c9865245e23649
jschless <joe.c.schlessinger@gmail.com>
2024-12-18 08:03:22 -0500

    day 18 solved

--- src/day18.py
+++ src/day18.py
@@ -1,5 +1,3 @@
-import heapq
-from collections import defaultdict
 from collections import deque
 
 fname = "./examples/day18.txt"
@@ -8,45 +6,42 @@ HT, WD = 6, 6
 N_STEPS = 12
 
 
-# fname = "./inputs/day18.txt"
-# HT, WD = 70, 70
-# N_STEPS = 1024
-# end = (70, 70)
+fname = "./inputs/day18.txt"
+HT, WD = 70, 70
+N_STEPS = 1024
+end = (70, 70)
 
 corrupted = set()
 start = (0, 0)
+to_add = []
 with open(fname) as f:
-
     for i, coord in enumerate(f.read().strip().split()):
-        if i == N_STEPS:
-            break
         x, y = coord.split(",")
-        corrupted.add((int(y), int(x)))
+        if i < N_STEPS:
+            corrupted.add((int(y), int(x)))
+        to_add.append((int(y), int(x)))
 
 
 def neighbors(y, x):
-    cand = []
-    for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+    return [
+        (y + dy, x + dx)
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]
         if (
             y + dy <= HT
             and y + dy >= 0
             and x + dx <= WD
             and x + dx >= 0
             and (y + dy, x + dx) not in corrupted
-        ):
-            cand.append((y + dy, x + dx))
-    return cand
+        )
+    ]
 
 
 def bfs(start, end):
-    visited = set()
-    q = deque()
-    q.append((0, start))
-    visited.add(start)
+    visited = set([start])
+    q = deque([(0, start)])
     while q:
         cost, loc = q.popleft()
         if loc == end:
-            print(loc)
             return cost
         for n in neighbors(*loc):
             if n not in visited:
@@ -56,3 +51,9 @@ def bfs(start, end):
 
 
 print("Part 1:", bfs(start, end))
+corrupted = set()
+for i, corr in enumerate(to_add):
+    corrupted.add(corr)
+    if bfs(start, end) == -1:
+        print("Part 2:", (corr[1], corr[0]))
+        break
