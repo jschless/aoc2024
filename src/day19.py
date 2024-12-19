1e2a0b0d0872d18dfba09f57480baabf6e474651
jschless <joe.c.schlessinger@gmail.com>
2024-12-19 07:45:34 -0500

    i credit/blame dr o for my default urge to use brute force recursion

--- src/day19.py
+++ src/day19.py
@@ -1,9 +1,6 @@
-from collections import deque
-
 fname = "./examples/day19.txt"
-
 fname = "./inputs/day19.txt"
-patterns = set()
+
 with open(fname) as f:
     p, d = f.read().strip().split("\n\n")
     patterns = set(p.split(", "))
@@ -14,33 +11,30 @@ MAX_LEN = max(len(s) for s in patterns)
 memo = {}
 
 
-def legit_design(d, builder):
-    # Go through string and store pottential patterns in builder
-    # If builder is a pattern, reset builder
-    # print("Call to", d, builder)
+def count_designs(d, builder):
     if (d, builder) in memo:
         return memo[(d, builder)]
 
     if len(d) == 0:
-        return builder == "" or builder in patterns
+        return 1 if builder == "" or builder in patterns else 0
     elif builder in patterns:
-        ans = legit_design(d[1:], d[0]) or legit_design(d[1:], builder + d[0])
+        ans = count_designs(d[1:], d[0]) + count_designs(d[1:], builder + d[0])
         memo[(d, builder)] = ans
         return ans
     elif builder == "" or len(builder) < MAX_LEN:
-        ans = legit_design(d[1:], builder + d[0])
+        ans = count_designs(d[1:], builder + d[0])
         memo[(d, builder)] = ans
         return ans
     else:
-        return False
+        return 0
 
 
-p1 = 0
+p1, p2 = 0, 0
 for d in designs:
-    # print(d, possible_designs(d, ""))
-    # p1 += possible_designs(d, ""
-    print(d, legit_design(d, ""))
-    if legit_design(d, ""):
+    ans = count_designs(d, "")
+    if ans > 0:
         p1 += 1
-    # break
+        p2 += ans
+
 print("Part 1:", p1)
+print("Part 2:", p2)
